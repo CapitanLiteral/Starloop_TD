9c2b8624152425d4ca3217a4959d92ce5606ee4f
CapitanLiteral <anaraguen@gmail.com>
2018-09-04 10:15:13 +0200

    Initialize done
    Pool manager 50% done
    
    Signed-off-by: CapitanLiteral <anaraguen@gmail.com>

--- Starloop_TD/Assets/Scripts/Managers/PoolManager.cs
+++ Starloop_TD/Assets/Scripts/Managers/PoolManager.cs
@@ -12,8 +12,8 @@ public class PoolManager : MonoBehaviour
 	[Serializable]
 	public struct Prefab
 	{
-		public int nInstances;
-		public GameObject prefab;
+		public int amount;
+		public GameObject objectPrefab;
 	}
 
 	//Type of accepted prefabs
@@ -32,7 +32,16 @@ public class PoolManager : MonoBehaviour
 	//Array that stores prefabs to be loaded on awake
 	public Prefab[] prefabs;
 
+	// Stores the available pooled objects
+	public List<GameObject>[] pooledObjects;
 
+	GameObject containerObject;
+
+	void Awake()
+	{
+		instance = this;
+		Initialize();
+	}
 
 	// Use this for initialization
 	void Start ()
@@ -44,5 +53,43 @@ public class PoolManager : MonoBehaviour
 	void Update ()
     {
 		
+	}
+
+
+
+	void Initialize()
+	{
+		containerObject = new GameObject("PoolManager");
+
+		pooledObjects = new List<GameObject>[prefabs.Length];
+
+		for (int i = 0; i < prefabs.Length; i++)
+		{
+			Prefab prefab = prefabs[i];
+			pooledObjects[i] = new List<GameObject>();
+
+			for (int n = 0; n < prefab.amount; n++)
+			{
+				GameObject newObj = Instantiate(prefab.objectPrefab) as GameObject;
+				newObj.name = prefab.objectPrefab.name;
+				PoolObject(newObj);
+			}
+		}
+	}
+
+
+	// Pools the object specified.  Will not be pooled if there is no prefab of that type.
+	public void PoolObject(GameObject obj)
+	{
+		for (int i = 0; i < prefabs.Length; i++)
+		{
+			if (prefabs[i].objectPrefab.name == obj.name)
+			{
+				obj.SetActive(false);
+				obj.transform.parent = containerObject.transform;
+				pooledObjects[i].Add(obj);
+				return;
+			}
+		}
 	}
 }
