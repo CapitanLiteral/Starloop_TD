21747b0eab293c00050d1ac2e62a292973524a27
capitanliteral <anaraguen@gmail.com>
2018-09-23 13:35:01 +0200

    Panning almost done, need to fix camera axis

--- Starloop_TD/Assets/Scripts/Camera/PanCamera.cs
+++ Starloop_TD/Assets/Scripts/Camera/PanCamera.cs
@@ -4,13 +4,42 @@ using UnityEngine;
 
 public class PanCamera : MonoBehaviour
 {
-
+	[SerializeField]
+	MapManager Map;
 	Vector2 MaxDistance;
+	[SerializeField]
+	float PanningSensitivity = 4f;
+	[SerializeField]
+	float PanningDampening = 2f;
 
-	
+	private void Start()
+	{
+		if (Map != null)
+		{
+			MaxDistance = Map.GetWorldSize() / 2;
+		}
+		
+	}
+
 	// Update is called once per frame
 	void LateUpdate ()
 	{
-		
+		float xAxisValue = Input.GetAxis("Horizontal");
+		float zAxisValue = Input.GetAxis("Vertical");
+
+		transform.Translate(new Vector3(xAxisValue, 0.0f, zAxisValue));
+
+		Vector3 pos = transform.position;
+
+		pos.x = Mathf.Clamp(pos.x, -MaxDistance.x, MaxDistance.x);
+		pos.z = Mathf.Clamp(pos.z, -MaxDistance.y, MaxDistance.y);
+
+		transform.position = pos;
+	}
+
+	private void OnDrawGizmos()
+	{
+		Gizmos.color = Color.cyan;
+		Gizmos.DrawCube(transform.position, new Vector3(1f, 1f, 1f));
 	}
 }
